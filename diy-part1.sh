57184d0b8d25efe770139519ce51d68de8c77f80
HuangZuoCan <69742931+a492219408@users.noreply.github.com>
2023-04-28 12:27:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,9 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.254/g' openwrt/package/base-files/files/bin/config_generate
+
+# Clear the login password
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
