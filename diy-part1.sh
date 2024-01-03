25be693858e60d0eb6a3130e2ac0de9cead7d808
YmdElf <a492219408@163.com>
2024-01-03 20:46:41 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,23 +19,23 @@
 
 rm -rf feeds.conf.default
 {
-#src-git packages https://git.openwrt.org/feed/packages.git^8e3a1824645f5e73ec44c897ac0755c53fb4a1f8
-#src-git luci https://git.openwrt.org/project/luci.git^7739e9f5b03b830f51d53c384be4baef95054cb3
-src-git routing https://git.openwrt.org/feed/routing.git^83ef3784a9092cfd0a900cc28e2ed4e13671d667
-src-git telephony https://git.openwrt.org/feed/telephony.git^9746ae8f964e18f04b64fbe1956366954ff223f8
+#echo 'src-git packages https://git.openwrt.org/feed/packages.git^8e3a1824645f5e73ec44c897ac0755c53fb4a1f8'
+#echo 'src-git luci https://git.openwrt.org/project/luci.git^7739e9f5b03b830f51d53c384be4baef95054cb3'
+echo 'src-git routing https://git.openwrt.org/feed/routing.git^83ef3784a9092cfd0a900cc28e2ed4e13671d667'
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git^9746ae8f964e18f04b64fbe1956366954ff223f8'
 
-src-git packages https://github.com/coolsnowwolf/packages
-src-git luci https://github.com/coolsnowwolf/luci
-#src-git routing https://github.com/coolsnowwolf/routing
-#src-git helloworld https://github.com/fw876/helloworld.git
-#src-git oui https://github.com/zhaojh329/oui.git
-#src-git video https://github.com/openwrt/video.git
-#src-git targets https://github.com/openwrt/targets.git
-#src-git oldpackages http://git.openwrt.org/packages.git
-#src-link custom /usr/src/openwrt/custom-feed
+echo 'src-git packages https://github.com/coolsnowwolf/packages'
+echo 'src-git luci https://github.com/coolsnowwolf/luci'
+#echo 'src-git routing https://github.com/coolsnowwolf/routing'
+#echo 'src-git helloworld https://github.com/fw876/helloworld.git'
+#echo 'src-git oui https://github.com/zhaojh329/oui.git'
+#echo 'src-git video https://github.com/openwrt/video.git'
+#echo 'src-git targets https://github.com/openwrt/targets.git'
+#echo 'src-git oldpackages http://git.openwrt.org/packages.git'
+#echo 'src-link custom /usr/src/openwrt/custom-feed'
 
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'
+echo 'src-git small https://github.com/kenzok8/small'
 } >> feeds.conf.default
 
 # Modify default IP
